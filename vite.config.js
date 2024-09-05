f0209d95ae6e99e59efe7c88b689d45b3940843a
Williams Velasquez <williams.velasquez.rivas@gmail.com>
2024-09-05 09:25:43 -0400

    si

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-weather",
   plugins: [react()],
+  base: "/react-weather",
   build: {
     outDir: "./docs",
     emptyOutDir: true,
